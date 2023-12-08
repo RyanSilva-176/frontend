013f12c000a373119ddd32e51ab58320c003b4f7
Ryan <ryansilva1760@gmail.com>
2023-12-08 19:26:54 -0300

    First Commit

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,14 @@
+import { Tabs } from 'expo-router'
+
+export default function AppLayout() {
+  return (
+    <Tabs>
+        <Tabs.Screen
+            name="index"
+            options={{
+                headerShown: false
+            }}
+        />
+    </Tabs>
+  )
+  }
---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,16 @@
+import { View, Text } from 'react-native'
+
+
+export default function Home() {
+  return (
+    <View>
+        <Text className={styles.title}>Hello World</Text>
+    </View>
+  )
+}
+
+
+
+const styles = {
+    title: "text-xl font-bold text-red-500"
+}
